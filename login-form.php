ea25ffe9f5af1c6f54d0d9e84199cede7e845609
Firas Alnahhas <asfoosh2014@gmail.com>
2017-10-01 00:37:32 +0300

    ordinary login page

---
+++ login-form.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+                integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+                crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"
+                integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4"
+                crossorigin="anonymous"></script>
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css"
+              integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"
+                integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1"
+                crossorigin="anonymous"></script>
+        
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
+
+
+        <script type="text/javascript">
+            $(document).ready(function () {
+
+
+
+            });
+
+        </script>
+
+
+        <style type="text/css">
+
+
+            *
+            {
+                -moz-box-sizing:border-box;
+                -webkit-box-sizing:border-box;
+                box-sizing:border-box;
+                box-sizing:border-box;
+            }
+
+            body {
+                background-image: url(images/background.jpg);
+            }
+
+
+
+            .custom
+            {
+                height:400px;
+                border-color:azure;
+                border-width:thin;
+                background-color: whitesmoke;
+                position:relative;
+                top:100px;
+                left:30%;
+                box-shadow:1px 1px 1px azure
+            }
+
+            div.card-footer
+            {
+
+                margin-top:100px
+            }
+
+            .form-control-label
+            {
+                font-weight:bold;
+                font-style: oblique;
+                font-size:1.2em;
+                line-height: 40px;
+                color:slategrey
+
+            }
+            
+     
+            div.card-block
+            {
+                margin-top:40px    
+            }
+
+
+        </style>
+
+    </head>
+    <body>
+
+
+
+
+        <div class="card custom col-xs-5 col-sm-5 col-md-5 col-lg-5" id="mainCard">
+
+            <div class="card-header">
+                <img src="images/logo.jpg" style="width:auto;height:40px" class="img-fluid card-img-top"/>
+            </div>
+            <div class="card-block">
+
+                <form role="form" method="get" action="">
+
+
+
+                    <div class="input-group ">
+                        <label for="user-email" class="form-control-label col-xs-3 col-sm-3 col-md-3 col-lg-3">E-mail:</label>
+                        <input class="form-control col-xs-9 col-sm-9 col-md-9 col-lg-9" id="user-email" name="user-email" type="email" placeholder="your email..."  required="required"/>
+                       <i class="input-group-addon">@</i>
+                    </div>
+
+                    <br/>
+                    <div class="input-group ">
+                        <label for="password" class="form-control-label col-xs-3 col-sm-3 col-md-3 col-lg-3">Password:</label>
+                        <input class="form-control col-xs-9 col-sm-9 col-md-9 col-lg-9"  id="pass" name="pass" type="password" placeholder="password here..." required="required"/>
+                         <i class="input-group-addon fa fa-key"></i>
+                    </div>
+
+
+                    <div class="card-footer">
+                        <input class="btn btn-secondary" type="submit" value="Submit"/>
+                        <input class="btn btn-secondary" type="reset" value="Clear"/>
+                    </div>
+                </form>
+
+            </div>
+
+        </div>
+
+
+    </body>
+
+
+</html>
+
+
+
